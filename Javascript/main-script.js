a159537e1cc1ededac93bd9be9e03cc8433e5564
Kavaughn <kavaughnawsomeness4@gmail.com>
2019-06-25 19:05:26 -0400

    javascript changes

--- Javascript/main-script.js
+++ Javascript/main-script.js
@@ -1,1 +1,15 @@
- 
+$("#search").on("click",function(){
+    alert("works!");
+    var APIKey = "kF0IDN1pYNuH8ZYf3jf2yQEPcNC2CHjG";
+    var queryURL = "https://rest.bandsintown.com/artists/" + "Chris Brown" + "?app_id=codingbootcamp;"
+
+
+      
+    // We then created an AJAX call
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        });
+}); 
